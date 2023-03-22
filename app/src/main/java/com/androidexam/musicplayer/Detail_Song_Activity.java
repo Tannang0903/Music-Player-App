805f778079f1c1f70f5cf686a78969363cdef081
duyquang2301 <duyquang.td7@gmail.com>
2023-03-23 06:42:09 +0700

    Detail_Song

---
+++ app/src/main/java/com/androidexam/musicplayer/Detail_Song_Activity.java
@@ -0,0 +1,14 @@
+package com.androidexam.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detail_Song_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_song);
+    }
+}
