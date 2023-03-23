8f68db14540286f26ff31ad1adc7f3fdbae7ee2b
leductai <ldtai26112002@gmail.com>
2023-03-23 11:55:00 +0700

    push demo lyric's view

--- app/src/main/java/com/androidexam/musicplayer/Detail_Song_Activity.java
+++ app/src/main/java/com/androidexam/musicplayer/Detail_Song_Activity.java
@@ -2,6 +2,8 @@ package com.androidexam.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Detail_Song_Activity extends AppCompatActivity {
