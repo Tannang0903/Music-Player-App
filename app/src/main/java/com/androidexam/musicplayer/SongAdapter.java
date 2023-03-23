5eb1899bbbade0873b9d2dcc1e87f43bb491de47
= <=>
2023-03-23 12:18:52 +0700

    feat: Main UI

---
+++ app/src/main/java/com/androidexam/musicplayer/SongAdapter.java
@@ -0,0 +1,76 @@
+package com.androidexam.musicplayer;
+
+import android.content.Context;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> {
+
+    public SongAdapter(Context mContext, ArrayList<Song> mSong) {
+        this.mContext = mContext;
+        this.mSong = mSong;
+    }
+
+    private Context mContext;
+    private ArrayList<Song> mSong;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.app_song_item, parent);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.file_name.setText(mSong.get(position).getTitle());
+        byte[] image = new byte[0];
+        try {
+            image = getAlbumArt(mSong.get(position).getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(image != null) {
+            Glide.with(mContext).asBitmap().load(image).into(holder.album_art);
+        } else {
+            Glide.with(mContext).load(R.drawable.gredient_bg).into(holder.album_art);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSong.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView file_name;
+        ImageView album_art;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            file_name = itemView.findViewById(R.id.song_file_name);
+            album_art = itemView.findViewById(R.id.song_image);
+        }
+    }
+
+    private byte[] getAlbumArt(String uri) throws IOException {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+
+}
