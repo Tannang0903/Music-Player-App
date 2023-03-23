1017099ef9309376e356e9ca51b9547b6953842f
leductai <ldtai26112002@gmail.com>
2023-03-23 14:12:29 +0700

    modified MainActivity

--- app/src/main/java/com/androidexam/musicplayer/MainActivity.java
+++ app/src/main/java/com/androidexam/musicplayer/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_lyric);
+        setContentView(R.layout.activity_detail_song);
 
     }
 }
